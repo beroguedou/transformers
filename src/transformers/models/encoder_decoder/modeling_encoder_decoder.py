049e174d1721b283d72161fa2e3166ae67109969
Béranger <50379085+beroguedou@users.noreply.github.com>
2021-03-01 09:04:36 +0100

    Update modeling_encoder_decoder.py

--- src/transformers/models/encoder_decoder/modeling_encoder_decoder.py
+++ src/transformers/models/encoder_decoder/modeling_encoder_decoder.py
@@ -427,7 +427,7 @@ class EncoderDecoderModel(PreTrainedModel):
             #use_cache=use_cache,
             #past_key_values=past_key_values,
             return_dict=return_dict,
-            #**kwargs_decoder,
+            **kwargs_decoder,
         )
 ######
 
@@ -443,7 +443,7 @@ class EncoderDecoderModel(PreTrainedModel):
         return Seq2SeqLMOutput(
             loss=decoder_outputs.loss,
             logits=decoder_outputs.logits,
-            past_key_values=decoder_outputs.past_key_values,
+            #past_key_values=decoder_outputs.past_key_values,
             decoder_hidden_states=decoder_outputs.hidden_states,
             decoder_attentions=decoder_outputs.attentions,
             cross_attentions=decoder_outputs.cross_attentions,
